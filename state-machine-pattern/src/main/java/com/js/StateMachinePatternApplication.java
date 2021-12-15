cfba886adc32707e76845709b6cf394cac76c063
姜爽 <2545251075@qq.com>
2021-12-15 21:43:22 +0800

    状态机模式初始化学习

--- state-machine-pattern/src/main/java/com/js/StateMachinePatternApplication.java
+++ state-machine-pattern/src/main/java/com/js/StateMachinePatternApplication.java
@@ -1,5 +1,7 @@
 package com.js;
 
+import cn.hutool.extra.spring.SpringUtil;
+import com.js.spring.service.IOrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,22 @@ public class StateMachinePatternApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StateMachinePatternApplication.class, args);
+
+        IOrderService orderService = SpringUtil.getBean("orderService");
+        orderService.create();
+        orderService.create();
+        orderService.pay(1);
+        new Thread("客户线程") {
+            @Override
+            public void run() {
+                orderService.deliver(1);
+                orderService.receive(1);
+            }
+        }.start();
+        orderService.pay(2);
+        orderService.deliver(2);
+        orderService.receive(2);
+        System.out.println("全部订单状态：" + orderService.getOrders());
     }
 
 }
---
+++ state-machine-pattern/src/main/java/com/js/spring/service/impl/OrderServiceImpl.java
@@ -0,0 +1,93 @@
+package com.js.spring.service.impl;
+
+import com.js.spring.domain.Order;
+import com.js.spring.enums.OrderStatus;
+import com.js.spring.enums.OrderStatusChangeEvent;
+import com.js.spring.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.persist.StateMachinePersister;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service("orderService")
+public class OrderServiceImpl implements IOrderService {
+    @Autowired
+    private StateMachine<OrderStatus, OrderStatusChangeEvent> orderStateMachiner;
+    @Autowired
+    private StateMachinePersister<OrderStatus, OrderStatusChangeEvent, Order> persister;
+
+    private int id = 1;
+    private Map<Integer, Order> orders = new HashMap<>();
+
+    @Override
+    public Order create() {
+        Order order = new Order();
+        order.setStatus(OrderStatus.WAIT_PAYMENT);
+        order.setId(id++);
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    @Override
+    public Order pay(int id) {
+        Order order = orders.get(id);
+        System.out.println("线程名称：" + Thread.currentThread().getName() + " 尝试支付，订单号：" + id);
+        Message message = MessageBuilder.withPayload(OrderStatusChangeEvent.PAYED).setHeader("order", order).build();
+        if (!sendEvent(message, order)) {
+            System.out.println("线程名称：" + Thread.currentThread().getName() + " 支付失败, 状态异常，订单号：" + id);
+        }
+        return orders.get(id);
+    }
+
+    @Override
+    public Order deliver(int id) {
+        Order order = orders.get(id);
+        System.out.println("线程名称：" + Thread.currentThread().getName() + " 尝试发货，订单号：" + id);
+        if (!sendEvent(MessageBuilder.withPayload(OrderStatusChangeEvent.DELIVERY).setHeader("order", order).build(), orders.get(id))) {
+            System.out.println("线程名称：" + Thread.currentThread().getName() + " 发货失败，状态异常，订单号：" + id);
+        }
+        return orders.get(id);
+    }
+
+    @Override
+    public Order receive(int id) {
+        Order order = orders.get(id);
+        System.out.println("线程名称：" + Thread.currentThread().getName() + " 尝试收货，订单号：" + id);
+        if (!sendEvent(MessageBuilder.withPayload(OrderStatusChangeEvent.RECEIVED).setHeader("order", order).build(), orders.get(id))) {
+            System.out.println("线程名称：" + Thread.currentThread().getName() + " 收货失败，状态异常，订单号：" + id);
+        }
+        return orders.get(id);
+    }
+
+    @Override
+    public Map<Integer, Order> getOrders() {
+        return orders;
+    }
+
+    /**
+     * 发送订单状态转换事件     *     * @param message     * @param order     * @return
+     */
+    private synchronized boolean sendEvent(Message<OrderStatusChangeEvent> message, Order order) {
+        boolean result = false;
+        try {
+            //尝试恢复状态机状态
+            orderStateMachiner.start();
+            //添加延迟用于线程安全测试
+            persister.restore(orderStateMachiner, order);
+            Thread.sleep(1000);
+            //持久化状态机状态
+            result = orderStateMachiner.sendEvent(message);
+            persister.persist(orderStateMachiner, order);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            orderStateMachiner.stop();
+        }
+        return result;
+    }
+}

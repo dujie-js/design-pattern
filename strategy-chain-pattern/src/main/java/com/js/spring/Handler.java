e4f2b332f023e4c1cdf95e6895f67483f64d0bd9
jiangshuang <jiangshuang@kanzhun.com>
2022-08-30 21:27:12 +0800

    临时保存

--- strategy-chain-pattern/src/main/java/com/js/spring/Handler.java
+++ strategy-chain-pattern/src/main/java/com/js/spring/Handler.java
@@ -2,14 +2,14 @@ package com.js.spring;
 
 import com.js.spring.intefaces.MyOrder;
 
-public abstract class Handler implements MyOrder {
-    protected Handler nextHandler;
+public abstract class Handler<S,R> implements MyOrder {
+    protected Handler<S,R> nextHandler;
 
-    public abstract void doHandle(Integer num);
+    public abstract S doHandle(R request);
 
-    public abstract Handler getNextHandler();
+    public abstract Handler<S,R> getNextHandler();
 
-    public void setNextHandler(Handler handler) {
+    public void setNextHandler(Handler<S,R> handler) {
         this.nextHandler = handler;
     }
 }
---
+++ strategy-chain-pattern/src/main/java/com/js/spring/TestHanadler.java
@@ -0,0 +1,5 @@
+package com.js.spring;
+
+public class TestHanadler {
+
+}
